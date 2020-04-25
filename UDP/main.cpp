1706f5968af9b5eab7805b20d3ec1d0a74f94e9b
AKosmachyov <a.kosmachyov@gmail.com>
2020-04-25 15:37:23 +0300

    Initial Commit

---
+++ UDP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UDP
+//
+//  Created by Alexander Kosmachyov on 4/25/20.
+//  Copyright © 2020 Alexander Kosmachyov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
